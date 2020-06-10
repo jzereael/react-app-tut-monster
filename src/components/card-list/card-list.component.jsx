abe2a3ff27864cdc576b6cf03eafabf3280d2b0b
Jesse <jsebaltazar@gmail.com>
2020-06-10 17:26:45 -0600

    Monster Rolodex Tutorial

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {Card} from '../card/card.component'
+
+import './card-list.styles.css'
+
+
+export const CardList = props => (
+
+<div className='card-list'>
+    {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster}/>
+       
+    ))}
+
+{props.monsters.map(monster => (console.log(monster.id,monster.name))   
+    )}
+</div>
+
+);
