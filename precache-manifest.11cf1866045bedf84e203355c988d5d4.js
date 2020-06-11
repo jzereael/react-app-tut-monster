69c61c75b9ab216db9e7fa7ea1e2c559683500e2
Jesse <jsebaltazar@gmail.com>
2020-06-10 18:20:24 -0600

    Updates

---
+++ precache-manifest.11cf1866045bedf84e203355c988d5d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f914af670b4acd8e50c462e437d93fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfbd6938e5c63fe2223",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71de33232228ef7849a5",
+    "url": "/static/js/2.e41982c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e41982c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfbd6938e5c63fe2223",
+    "url": "/static/js/main.8faade6e.chunk.js"
+  },
+  {
+    "revision": "a8a80d3d96a29d231572",
+    "url": "/static/js/runtime-main.ce297290.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
